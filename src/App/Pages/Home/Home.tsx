107793b67c42dceb8a3f30edb2730f08122be0af
neathcmd <ksdf71464@gmail.com>
2025-03-31 16:29:26 +0700

    final for the day

--- src/App/Pages/Home/Home.tsx
+++ src/App/Pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/Hero";
+import Highlight from "./components/Highlight";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <Highlight />
     </div>
   );
 };
---
+++ src/App/Pages/Home/components/Highlight.tsx
@@ -0,0 +1,12 @@
+export default function Highlight() {
+  return (
+    <section className="w-full min-h-screen flex items-center">
+      <div>
+        {/* head */}
+        <div>
+          <h1>Discover our content</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
