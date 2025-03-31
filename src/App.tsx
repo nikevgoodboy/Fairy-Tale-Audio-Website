a6585dbd7097d1b3f2e57dadfa5584d03d516ebf
neathcmd <ksdf71464@gmail.com>
2025-03-31 10:08:02 +0700

    complete navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { ThemeProvider } from "./App/Ui/ThemeContext";
 import Navbar from "./components/Navbar";
-import Home from "./App/Home/Home";
-    import Footer from "./components/Footer"
+import Home from "./App/Pages/Home/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <main>
         <Home />
       </main>
-      <Footer/>
+      <Footer />
     </ThemeProvider>
   );
 }
