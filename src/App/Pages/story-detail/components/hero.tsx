22f7b86c5d2ca8ad5b5876aa14fd8662f282e83b
thounsova <thoun.sovannareach@institute.pse.ngo>
2025-04-02 16:16:20 +0700

    add story

--- src/App/Pages/story-detail/components/hero.tsx
+++ src/App/Pages/story-detail/components/hero.tsx
@@ -1,7 +1,16 @@
 export default function Hero() {
   return (
     <section>
-      <p>Hero Favorite</p>
+      <div className=" from-teal-100 to-lime-100 p-8 rounded-lg ">
+        <div className="text-center">
+          <h1 className="text-5xl font-extrabold text-withe mb-2">
+            FAIRY TALES
+          </h1>
+          <h1 className="text-3xl font-semibold text-pink-600 mb-4">
+            Tales for Young
+          </h1>
+        </div>
+      </div>
     </section>
   );
 }
