5402b132ffe3548b2b10680681791ab47b561cf6
neathcmd <ksdf71464@gmail.com>
2025-04-02 16:23:19 +0700

    complete home page tmr

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import Favorite from "./App/Pages/Favorite/Favorite";
 import StoryDetail from "./App/Pages/story-detail/story-detail";
+// i dont now
 
 function App() {
   return (
