91752d62333b06424e3a72511d168aecd1902f3a
John Doe <johndoe@example.com>
2025-04-03 08:10:02 +0700

    add Favorite

--- src/App/Pages/Favorite/components/Hero.tsx
+++ src/App/Pages/Favorite/components/Hero.tsx
@@ -1,7 +1,10 @@
+
 export default function Hero() {
   return (
-    <section>
-      <p>Hero section Favorite</p>
+    <section className="text-center p-5">
+        <h1 className=" text-4xl md:text-6xl font-extrabold tracking-tight bg-gradient-to-r from-pink-500 to-purple-600 bg-clip-text text-transparent p-4 rounded-lg shadow-lg">
+          FAVORITE
+        </h1>
     </section>
   );
 }
